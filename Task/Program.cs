fc76d3f1c41efdf4fa4f187e2acd3a7f31910daa
NikolayKartaev <kartaevgeek@gmail.com>
2023-03-10 15:35:00 +0300

    Added solved task

---
+++ Task/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+Console.Write("Введите некоторый текст: ");
+string[] text = Console.ReadLine()!.Split().ToArray();
+Console.WriteLine();
+Console.WriteLine($"Первоначальный массив строк: [{String.Join(", ", text)}]");
+
+int count = 0;
+for (int i = 0; i < text.Length; i++)
+{
+    if (text[i].Length <= 3)
+    {
+        count++;
+    }
+}
+
+string[] text2 = new string[count];
+for (int j = 0; j < text2.Length; j++)
+{
+    for (int k = 0; k < text.Length; k++)
+    {
+        if (text[k].Length <= 3)
+        {
+            text2[j] = text[k];
+            j++;
+        }
+    }
+}
+Console.WriteLine();
+
+Console.Write($"Итоговый массив строк: [{String.Join(", ", text2)}]");
+
